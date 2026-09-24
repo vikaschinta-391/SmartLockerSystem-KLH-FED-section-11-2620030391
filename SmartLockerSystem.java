20c4e2f5b597ada7c2bdbec8cb32b8e6623c2acd
vikaschinta-391 <vikaschinta@klh.edu.in>
2026-09-24 14:53:20 +0530

    Update SmartLockerSystem.java

--- SmartLockerSystem.java
+++ SmartLockerSystem.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
+import java.util.Random;
 
+public class SmartLockerSystem{
+  //array to hold 5 lockers 
+  static Locker[] lockers = new Locker[5];
+  static random = new Random();
+
+  public static void main(String[] args) {
+        // Initialize the 5 lockers
+        for (int i = 0; i < lockers.length; i++) {
+            lockers[i] = new Locker(i + 1);
+        }
